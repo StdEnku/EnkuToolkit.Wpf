ca9c45a04af998fd978f3028e17bfa42b918d16e
StdEnku <SyplessEleki@gmail.com>
2023-02-23 05:20:25 +0900

    ApplicationPropertyiesService追加

---
+++ EnkuToolkit.Wpf/Services/ApplicationPropertyiesService.cs
@@ -0,0 +1,16 @@
+﻿namespace EnkuToolkit.Wpf.Services;
+
+using EnkuToolkit.UiIndependent.Services;
+using System.Collections;
+using System.Windows;
+
+/// <summary>
+/// Application.PropertiesプロパティをViewModelから操作可能にするためのViewService
+/// </summary>
+public class ApplicationPropertyiesService : IApplicationPropertyiesService
+{
+    /// <summary>
+    /// Application.Propertiesプロパティを取得するためのプロパティ
+    /// </summary>
+    public IDictionary Properties => Application.Current.Properties;
+}
