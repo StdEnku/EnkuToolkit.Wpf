00fd176fa6af2b47f8652dd1535cd77cf2aa3c4d
StdEnku <SyplessEleki@gmail.com>
2023-03-23 12:22:27 +0900

    不要な属性削除

--- EnkuToolkit.UiIndependent/Items/CalendarDayOfWeekSource.cs
+++ EnkuToolkit.UiIndependent/Items/CalendarDayOfWeekSource.cs
@@ -8,19 +8,16 @@ public class CalendarDayOfWeekSource
     /// <summary>
     /// 設定対象の曜日
     /// </summary>
-    [System.ComponentModel.Bindable(true)]
     public DayOfWeek TargetDayOfWeek { get; init; }
 
     /// <summary>
     /// 表示するテキスト
     /// </summary>
-    [System.ComponentModel.Bindable(true)]
     public string Text { get; init; } = string.Empty;
 
     /// <summary>
     /// 対象の曜日が休日がどうかを取得するためのプロパティ
     /// </summary>
-    [System.ComponentModel.Bindable(true)]
     public bool IsHoliday => this.TargetDayOfWeek == DayOfWeek.Saturday ||
                              this.TargetDayOfWeek == DayOfWeek.Sunday ?
                              true : false;
