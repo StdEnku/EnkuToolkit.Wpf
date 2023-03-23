ee744051381e3d3e11128c43088f7c7aa3e6ff95
StdEnku <SyplessEleki@gmail.com>
2023-03-24 07:38:32 +0900

    YearAndMonthを他のコントロールと同じ名前空間で使用可能なように再定義を実装

---
+++ EnkuToolkit.Wpf/Controls/ReDefinitions/CustamizableCalendar/YearAndMonth.cs
@@ -0,0 +1,11 @@
+﻿namespace EnkuToolkit.Wpf.Controls.ReDefinitions.CustamizableCalendar;
+
+using EkuiYearAndMonth = EnkuToolkit.UiIndependent.CustamizableCalendarDatas.YearAndMonth;
+
+/// <summary>
+/// 他のコントロールと同じxml名前空間で呼び出し可能なCalendarSourceクラス
+/// </summary>
+public class YearAndMonth : EkuiYearAndMonth
+{
+
+}
