bb08158dac6945e164059acfeff6caac12f5bd52
StdEnku <SyplessEleki@gmail.com>
2023-04-03 23:59:04 +0900

    月を変更した際選択済みの項目がクリアされるように修正

---
+++ EnkuToolkit.Wpf/Controls/Internals/CustamizableCalendar/ListboxUnselectOnDataContextChangedBehavior.cs
@@ -0,0 +1,37 @@
+﻿namespace EnkuToolkit.Wpf.Controls.Internals.CustamizableCalendar;
+
+using System.Windows;
+using System.Windows.Controls;
+
+internal class ListboxUnselectOnDataContextChangedBehavior
+{
+    public static readonly DependencyProperty IsApplyProperty
+        = DependencyProperty.RegisterAttached(
+            "IsApply",
+            typeof(bool),
+            typeof(ListboxUnselectOnDataContextChangedBehavior),
+            new PropertyMetadata(false, onIsApplyChanged)
+        );
+
+    public static void SetIsApply(ListBox target, bool value)
+        => target.SetValue(IsApplyProperty, value);
+
+    public static bool GetIsApply(ListBox target)
+        => (bool)target.GetValue(IsApplyProperty);
+
+    private static void onIsApplyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        var listBox = (ListBox)d;
+        var value = (bool)e.NewValue;
+        if (value)
+            listBox.DataContextChanged += ListBox_DataContextChanged;
+        else
+            listBox.DataContextChanged -= ListBox_DataContextChanged;
+    }
+
+    private static void ListBox_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+    {
+        var listBox = (ListBox)sender;
+        listBox.UnselectAll();
+    }
+}
--- Sandbox/App.xaml.cs
+++ Sandbox/App.xaml.cs
@@ -13,8 +13,6 @@ public partial class App : Application, IServicesOwner
     public App()
     {
         this.Services = ConfigureServices();
-        AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
-        this.DispatcherUnhandledException += App_DispatcherUnhandledException;
         this.InitializeComponent();
     }
 
@@ -33,25 +31,5 @@ public partial class App : Application, IServicesOwner
         services.AddTransient<IApplicationPropertyiesService, ApplicationPropertyiesService>();
 
         return services.BuildServiceProvider();
-    }
-
-    // 
-    private void CurrentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
-    {
-        string errorMember = e.Exception.TargetSite.Name;
-        string errorMessage = e.Exception.Message;
-        string message = string.Format($"例外が{errorMember}で発生。\nプログラムは継続します。\nエラーメッセージ：{errorMessage}");
-        MessageBox.Show(message, "FirstChanceException", MessageBoxButton.OK, MessageBoxImage.Information);
-    }
-
-    // UIスレッドで発生した例外の補足用メソッド
-    private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-    {
-        string errorMember = e.Exception.TargetSite.Name;
-        string errorMessage = e.Exception.Message;
-        string message = string.Format($"例外が{errorMember}で発生。\nプログラムを継続しますか？\nエラーメッセージ：{errorMessage}");
-        MessageBoxResult result = MessageBox.Show(message, "DispatcherUnhandledException", MessageBoxButton.YesNo, MessageBoxImage.Warning);
-        if (result == MessageBoxResult.Yes)
-            e.Handled = true;
     }
 }
