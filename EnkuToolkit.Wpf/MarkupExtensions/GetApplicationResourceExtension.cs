1c6dca0311dd797ba4b6dadd6178924639378a7f
StdEnku <SyplessEleki@gmail.com>
2023-06-05 03:45:29 +0900

    カレンダーのセルにマテリアルデザインを適用できなかった問題を修正

---
+++ EnkuToolkit.Wpf/MarkupExtensions/GetApplicationResourceExtension.cs
@@ -0,0 +1,66 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2023 StdEnku
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+namespace EnkuToolkit.Wpf.MarkupExtensions;
+
+using System;
+using System.Windows;
+using System.Windows.Markup;
+
+/// <summary>
+/// Markup extension to retrieve resources in the Application class.
+/// </summary>
+public class GetApplicationResourceExtension : MarkupExtension
+{
+    private Object _key;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="key">Key of the resource you want to search.</param>
+    public GetApplicationResourceExtension(Object key)
+        => _key = key;
+
+    /// <summary>
+    /// Methods to create return values for this markup extension
+    /// </summary>
+    /// <returns>
+    /// Object of the discovered resource
+    /// Return null if not found
+    /// </returns>
+    public override object? ProvideValue(IServiceProvider serviceProvider)
+    {
+        object? result;
+
+        try
+        {
+            result = Application.Current.FindResource(_key);
+        }
+        catch (ResourceReferenceKeyNotFoundException)
+        {
+            result = null;
+        }
+
+        return result;
+    }
+}
