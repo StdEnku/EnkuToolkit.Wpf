9db30c6fcfae763194751ec96d1e11bb26cc2123
StdEnku <SyplessEleki@gmail.com>
2023-06-05 15:53:37 +0900

    整数値から月の名前に変換するためのコンバーターを追加

---
+++ EnkuToolkit.Wpf/Converters/NumberToCurrentCultureMonthNameConverter.cs
@@ -0,0 +1,50 @@
+﻿namespace EnkuToolkit.Wpf.Converters;
+
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+/// <summary>
+/// Converter for converting integers from 1 to 12 to strings with month names, such as January and February, based on CultureInfo.CurrentCulture
+/// </summary>
+[ValueConversion(typeof(int), typeof(String))]
+public class NumberToCurrentCultureMonthNameConverter : IValueConverter
+{
+    /// <summary>
+    /// Property to get an instance from x:Static
+    /// </summary>
+    public static NumberToCurrentCultureMonthNameConverter Instance => new NumberToCurrentCultureMonthNameConverter();
+
+    /// <summary>
+    /// Methods for Forward Conversion
+    /// </summary>
+    /// <param name="value">Binding Source Value</param>
+    /// <param name="targetType">Not used</param>
+    /// <param name="parameter">Not used</param>
+    /// <param name="culture">Not used</param>
+    /// <returns>Names of months such as January and February</returns>
+    /// <exception cref="ArgumentException">Exception thrown if the value of the binding source exceeds the range of values from 1 to 12</exception>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var num = (int)value;
+        if (num < 1 || num > 12)
+            throw new ArgumentOutOfRangeException(nameof(value), "When using the NumberToMonthNameConverter, the binding source can only have values from 1 to 12.");
+
+        return CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(num);
+    }
+
+    /// <summary>
+    /// Methods for reverse direction
+    /// This converter does not support reverse conversion
+    /// </summary>
+    /// <param name="value">Not used</param>
+    /// <param name="targetType">Not used</param>
+    /// <param name="parameter">Not used</param>
+    /// <param name="culture">Not used</param>
+    /// <returns>Not used</returns>
+    /// <exception cref="NotImplementedException">Exception thrown when a reverse transformation is performed</exception>
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new InvalidOperationException("NumberToCurrentCultureMonthNameConverter does not support reverse return");
+    }
+}
