e47596ef66b61f745b4660f4d035b03018a30bd7
StdEnku <SyplessEleki@gmail.com>
2023-06-06 21:27:45 +0900

    アニメーションの修正

--- Sandbox/ViewModels/Page1ViewModel.cs
+++ Sandbox/ViewModels/Page1ViewModel.cs
@@ -1,7 +1,10 @@
 ﻿namespace Sandbox.ViewModels;
 
+using EnkuToolkit.UiIndependent.Collections;
 using EnkuToolkit.UiIndependent.Navigation;
+using Sandbox.DataObjects;
 using Sandbox.MvvmUtil;
+using System;
 using System.Diagnostics;
 
 public class Page1ViewModel : ViewModelsBase, INavigationAware
@@ -11,5 +14,23 @@ public class Page1ViewModel : ViewModelsBase, INavigationAware
         Debug.WriteLine(navigationMode.ToString());
         var p = (string?)param;
         Debug.WriteLine(p);
+    }
+
+    private DayDataCollection _dayDataCollection = new(DateTime.Now.Year, DateTime.Now.Month)
+    {
+        new DayData(1, "Tater tots", "Cobb salad", "Pot roast"),
+        new DayData(3, "bread", "fish", "Fajitas"),
+        new DayData(5, "salmon", "fish", "beef"),
+        new DayData(7, "bread", "fish", "Jambalaya"),
+    };
+
+    public DayDataCollection DayDataCollection
+    {
+        get => _dayDataCollection;
+        set
+        {
+            _dayDataCollection = value;
+            NotifyPropertyChanged();
+        }
     }
 }
