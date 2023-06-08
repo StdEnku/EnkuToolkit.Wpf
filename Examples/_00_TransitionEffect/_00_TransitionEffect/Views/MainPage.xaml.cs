ba11c830d9ab762eba17fdb6d2b6adffa151f686
StdEnku <SyplessEleki@gmail.com>
2023-06-08 22:46:30 +0900

    add example

---
+++ Examples/_00_TransitionEffect/_00_TransitionEffect/Views/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _00_TransitionEffect.Views;
+
+using System.Windows.Controls;
+
+public partial class MainPage : Page
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Examples/_03_BindingViewModelPropertyToSelectedItems/_03_BindingViewModelPropertyToSelectedItems/MainWindowViewModel.cs
@@ -0,0 +1,66 @@
+﻿namespace _03_BindingViewModelPropertyToSelectedItems;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using EnkuToolkit.UiIndependent.Services;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[ViewModel]
+public partial class MainWindowViewModel : ObservableObject
+{
+    private IMessageBoxService _messageBoxService;
+
+    public MainWindowViewModel(IMessageBoxService messageBoxService)
+    {
+        _messageBoxService = messageBoxService;
+    }
+
+    [ObservableProperty]
+    private IList _itemsSource = new List<string>()
+    {
+        "apple",
+        "banana",
+        "grape",
+        "lemon",
+        "strawberry",
+        "peach",
+        "mango",
+        "papaya"
+    };
+
+    [ObservableProperty]
+    private IList _selectedItems = new List<string>();
+
+    [RelayCommand]
+    private void ShowSelectedItem()
+    {
+        var text = string.Empty;
+        var stringItems = SelectedItems.Cast<string>();
+        foreach (var item in stringItems)
+        {
+            text += item + '\n';
+        }
+
+        _messageBoxService.ShowOk(text, "selected items");
+    }
+
+    [RelayCommand]
+    private void AllSelect()
+    {
+        var nextSelectedItem = new List<object>()
+        {
+            "apple",
+            "banana",
+            "grape",
+            "lemon",
+            "strawberry",
+            "peach",
+            "mango",
+            "papaya"
+        };
+
+        SelectedItems = nextSelectedItem;
+    }
+}
